2002cc5594ad62a3909ad2ac64f04f2e3b31d7e1
Andrea Olivos <andrea.olivos.b@gmail.com>
2025-06-05 15:20:37 -0400

    Add getComputerChoice function

---
+++ rock-paper-scissors.js
@@ -0,0 +1,20 @@
+// Function for the computer to choose between rock, paper or scissors
+const getComputerChoice = () => {
+  let randomNumber = 0;
+  // Choose a random number from 1 to 9. As Math.random will go from 0 to 9, every time the choice is 0, try again.
+  while (randomNumber === 0) {
+    randomNumber = Math.floor( Math.random() * 10 );
+  }
+
+  // 1, 2 and 3 select "rock"
+  if (randomNumber <= 3) {
+    return "rock";
+  } // 7, 8 and 9 select "paper"
+  else if (randomNumber >= 7) {
+    return "paper";
+  } // 4, 5 and 6 select "scissors"
+  else {
+    return "scissors";
+  }
+}
+
